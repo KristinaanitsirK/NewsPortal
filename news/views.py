975b3a4c3f4f99d3609003a715489122679bdf1c
Kristina <kristina.nte@icloud.com>
2024-07-02 01:16:32 +0300

    modified news page

--- news/views.py
+++ news/views.py
@@ -4,13 +4,18 @@ from .models import *
 
 
 class PostList(ListView):
-    queryset = Post.objects.order_by('title')
-    template_name = 'news.html'
-    context_object_name = 'news'
+    model = Post
+    ordering = '-created_at'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 class PostDetail(DetailView):
     model = Post
-    template_name = 'article.html'
-    context_object_name = 'article'
+    template_name = 'news_detail.html'
+    context_object_name = 'post'
 

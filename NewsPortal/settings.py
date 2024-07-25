e84cdabbd3b0f26f5582758cde321ad56c8e71c1
Kristina <kristina.nte@icloud.com>
2024-07-25 22:36:38 +0300

    celery scheduler added

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -196,3 +196,17 @@ MANAGERS = [
 ]
 
 EMAIL_SUBJECT_PREFIX = 'NewsPortal'
+
+# Celery settings
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+
+CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_TIMEZONE = 'UTC'
